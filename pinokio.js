44ab67dadcc9163d1986deba0c33aedcf1d068a5
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-03 18:45:52 -0500

    reset

--- pinokio.js
+++ pinokio.js
@@ -44,7 +44,9 @@ module.exports = {
         }, {
           icon: "fa-solid fa-rotate", text: "Update", href: "update.json"
         }, {
-          icon: "fa-solid fa-plug", text: "Fresh reinstall", href: "install.json"
+          icon: "fa-solid fa-plug", text: "Reinstall", href: "install.json"
+        }, {
+          icon: "fa-solid fa-broom", text: "Reset", href: "reset.json"
         }]
       }
     } else {
